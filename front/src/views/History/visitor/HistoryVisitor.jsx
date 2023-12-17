9bd38448d09f77d3b17dfe307347d3cf322644d3
Yagya Adh <yagyaadhikari02@gmail.com>
2023-12-17 18:35:19 +0545

    install: installed yagya-quiz component package into application

--- front/src/views/History/visitor/HistoryVisitor.jsx
+++ front/src/views/History/visitor/HistoryVisitor.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Quize from "yagya-quiz";
 
 const HistoryVisitor = () => {
   const Person = {
@@ -9,40 +10,46 @@ const HistoryVisitor = () => {
   };
 
   return (
-    <div className="">
-      <div>
-        <input type="text" name="" id="" placeholder="search visitor" />
+    <div className="absolute right-5">
+      <div className="bg-green-600 p-5">
+        <Quize />
       </div>
+      <div className="relative flex">
+        <container>
+          <div>
+            <input type="text" name="" id="" placeholder="search visitor" />
+          </div>
 
-      <div className="flex">
-        <div className="border-2 m-2 p-1 col-span-4 ">
-          <img src="" alt="" />
-          <h1>Name: {Person.name}</h1>
-          <h1>Phone: {Person.phone}</h1>
-          <h1>Address: {Person.Address}</h1>
-        </div>
+          <div className="flex">
+            <div className="border-2 m-2 p-1 col-span-4 ">
+              <img src="" alt="" />
+              <h1>Name: {Person.name}</h1>
+              <h1>Phone: {Person.phone}</h1>
+              <h1>Address: {Person.Address}</h1>
+            </div>
 
-        <div className="border-2 m-2 p-1 col-span-4  ">
-          <img src="" alt="" />
-          <h1>Name: {Person.name}</h1>
-          <h1>Phone: {Person.phone}</h1>
-          <h1>Address: {Person.Address}</h1>
-        </div>
+            <div className="border-2 m-2 p-1 col-span-4  ">
+              <img src="" alt="" />
+              <h1>Name: {Person.name}</h1>
+              <h1>Phone: {Person.phone}</h1>
+              <h1>Address: {Person.Address}</h1>
+            </div>
 
-        <div className="border-2 m-2 p-1 col-span-4  ">
-          <img src="" alt="" />
-          <h1>Name: {Person.name}</h1>
-          <h1>Phone: {Person.phone}</h1>
-          <h1>Address: {Person.Address}</h1>
-        </div>
+            <div className="border-2 m-2 p-1 col-span-4  ">
+              <img src="" alt="" />
+              <h1>Name: {Person.name}</h1>
+              <h1>Phone: {Person.phone}</h1>
+              <h1>Address: {Person.Address}</h1>
+            </div>
 
-        <div className="border-2 m-2 p-1 col-span-4  ">
-          <img src="" alt="" />
-          <h1>Name: {Person.name}</h1>
-          <h1>Phone: {Person.phone}</h1>
-          <h1>Address: {Person.Address}</h1>
-        </div>
-        
+            <div className="border-2 m-2 p-1 col-span-4  ">
+              <img src="" alt="" />
+              <h1>Name: {Person.name}</h1>
+              <h1>Phone: {Person.phone}</h1>
+              <h1>Address: {Person.Address}</h1>
+            </div>
+          </div>
+        </container>
       </div>
     </div>
   );
